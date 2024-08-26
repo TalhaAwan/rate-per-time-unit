f2fbdcc026f4db3891aa07bfc23213dd073ebc89
TalhaAwan <talhamanzoorawan@gmail.com>
2024-08-27 00:13:16 +0500

    extend super class RatePerTimeUnit

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,70 @@
-class RatePerSecond {
-  private hash: Map<number, number> = new Map();
-  private window: number = 10;
+class RatePerTimeUnit {
+  protected hash: Map<number, number> = new Map();
+  protected window: number;
 
-  constructor(window?: number) {
-    if (window && Number.isInteger(window) && window > 0 && window <= 20) {
+  constructor(window: number = 10) {
+    if (Number.isInteger(window) && window > 0 && window <= 20) {
       this.window = window;
+    } else {
+      throw new Error("Window must be an integer between 1 and 20");
     }
   }
 
-  registerEvent() {
-    const thisSecond = Math.floor(new Date().getTime() / 1000);
+  protected registerEvent(timeUnit: number) {
+    const thisTimeUnit = Math.round(new Date().getTime() / (1000 * timeUnit));
 
-    if (this.hash.has(thisSecond)) {
-      this.hash.set(thisSecond, this.hash.get(thisSecond) as number + 1)
-    }
-    else {
+    if (this.hash.has(thisTimeUnit)) {
+      this.hash.set(thisTimeUnit, this.hash.get(thisTimeUnit) as number + 1);
+    } else {
       if (this.hash.size >= this.window) {
         this.hash.delete(this.hash.keys().next().value);
       }
-      this.hash.set(thisSecond, 1);
+      this.hash.set(thisTimeUnit, 1);
     }
   }
 
-  getRatePerSecond() {
-    const thisSecond = Math.floor(new Date().getTime() / 1000);
-    const windowStart = thisSecond - this.window;
+  protected getRate(timeUnit: number) {
+    const thisTimeUnit = Math.round(new Date().getTime() / (1000 * timeUnit));
+    const windowStart = thisTimeUnit - this.window;
 
     let count = 0;
-    let occurences = 0;
+    let occurrences = 0;
 
     for (const [key, value] of this.hash.entries()) {
       if (key >= windowStart) {
         count += value;
-        occurences++;
+        occurrences++;
       }
     }
 
-    if (!count) return 0;
 
-    return parseFloat((count / occurences).toFixed(2));
+    return {
+      count, occurrences
+    }
+  }
+}
+
+
+class RatePerSecond extends RatePerTimeUnit {
+  constructor(window?: number) {
+    if (!window) {
+      super(10);
+    }
+    else if (Number.isInteger(window) && window > 0 && window <= 20) {
+      super(window);
+    } else {
+      throw new Error("Sliding window for rate per second must be an integer between 1 and 20");
+    }
+  }
+
+  registerEvent() {
+    super.registerEvent(1);
+  }
+
+  getRatePerSecond() {
+    const { count, occurrences } = super.getRate(1);
+    if (!count) return 0;
+    return parseFloat((count / occurrences).toFixed(2));
   }
 }
 
